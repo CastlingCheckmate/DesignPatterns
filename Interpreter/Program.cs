e541bd172eaf166ceab67fc19808eaa66252bac7
Илья Ирбитский <CastlingCheckmate97@gmail.com>
2019-03-30 13:35:24 +0300

    Refactoring of .csproj and Program.cs

--- Interpreter/Program.cs
+++ Interpreter/Program.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Interpreter
+﻿namespace InterpreterPattern
 {
-    class Program
+
+    internal sealed class Program
     {
+
         static void Main(string[] args)
         {
+
         }
+
     }
+
 }
--- Memento/Program.cs
+++ Memento/Program.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Memento
+﻿namespace MementoPattern
 {
-    class Program
+
+    internal sealed class Program
     {
+
         static void Main(string[] args)
         {
+
         }
+
     }
+
 }
